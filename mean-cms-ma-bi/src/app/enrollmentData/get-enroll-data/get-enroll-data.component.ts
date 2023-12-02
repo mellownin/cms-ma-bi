1bd7f8bea4c56536dea6b04ce52d04e8499f58ef
Mellownyn <sunnyshettigar@gmail.com>
2023-12-02 12:53:40 -0600

    Add in get enrollment data components

---
+++ mean-cms-ma-bi/src/app/enrollmentData/get-enroll-data/get-enroll-data.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component ({
+    selector: 'app-get-enroll-data',
+    templateUrl: './get-enroll-data.component.html'
+})
+
+export class GetEnrollDataComponent {
+
+}
