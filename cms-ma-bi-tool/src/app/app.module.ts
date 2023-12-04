1e90113e4e93642092e2e0634aba75c429c28bb8
Mellownyn <sunnyshettigar@gmail.com>
2023-12-04 17:18:41 -0600

    Add component for getting CMS enrollment data

--- cms-ma-bi-tool/src/app/app.module.ts
+++ cms-ma-bi-tool/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GetCmsEnrollDataComponent } from './get-cms-enroll-data/get-cms-enroll-data.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GetCmsEnrollDataComponent
   ],
   imports: [
     BrowserModule,
---
+++ cms-ma-bi-tool/src/app/get-cms-enroll-data/get-cms-enroll-data.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-get-cms-enroll-data',
+  templateUrl: './get-cms-enroll-data.component.html',
+  styleUrl: './get-cms-enroll-data.component.css'
+})
+export class GetCmsEnrollDataComponent {
+
+}
