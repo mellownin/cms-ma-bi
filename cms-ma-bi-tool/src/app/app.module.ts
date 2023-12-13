f643db6bdd738f11b8440ea74d944418f5248500
Mellownyn <sunnyshettigar@gmail.com>
2023-12-13 15:51:59 -0600

    Remove unecessary components

--- cms-ma-bi-tool/src/app/app.module.ts
+++ cms-ma-bi-tool/src/app/app.module.ts
@@ -3,12 +3,10 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { GetCmsEnrollDataComponent } from './get-cms-enroll-data/get-cms-enroll-data.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    GetCmsEnrollDataComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
